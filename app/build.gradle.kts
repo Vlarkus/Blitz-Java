3175a6815f0be6b25d253b17b9b6cf93e0363bea
Vlarkus <valery.rabchanka@gmail.com>
2024-10-12 17:42:33 -0400

    JUnit 5 Dependency Configured

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     // This dependency is used by the application.
     implementation(libs.guava)
     implementation("com.google.code.gson:gson:2.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 }
 
 testing {
